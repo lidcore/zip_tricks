2422c19065241b96a798c37093b84a4404bcab80
Romain Beauxis <toots@rastageeks.org>
2017-10-12 15:16:19 -0500

    Typo.

--- lib/zip_tricks/streamer/async_writable.rb
+++ lib/zip_tricks/streamer/async_writable.rb
@@ -1,7 +1,7 @@
 # Gets yielded from the writing methods of the CompressingStreamer
 # and accepts the data being written into the ZIP. Executes given callbacks
 # when done.
-class ZipTricks::Streamer::AsyncWritable << ZipTricks::Streamer::Writable
+class ZipTricks::Streamer::AsyncWritable < ZipTricks::Streamer::Writable
   # Initializes a new Writable with the object it delegates the writes to.
   # Normally you would not need to use this method directly
   def initialize(writer)
